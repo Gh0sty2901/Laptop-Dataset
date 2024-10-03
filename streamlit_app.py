997798abb688daa67418f3a4dfb76a221631790e
Gh0sty2901 <manguerraandre@gmail.com>
2024-10-03 22:41:33 +0800

    Added CSV for laptop price readying the rest

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,3 +11,5 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+df = pd.read_csv('laptop_price - dataset.csv')
+
