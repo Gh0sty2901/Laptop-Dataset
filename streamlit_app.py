b9cb48f2891db77409560724461c9a1fa02b47dd
Gh0sty2901 <manguerraandre@gmail.com>
2024-10-04 11:41:45 +0800

    Requirements

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,30 @@ import altair as alt
 from wordcloud import WordCloud
 from mpl_toolkits.mplot3d import Axes3D
 
-st.title("🎈 My new app")
+st.title("GROUP 6 LAPTOP-PRICES DATA SET")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 df = pd.read_csv('laptop_price - dataset.csv')
+
+#TABLE 1
+
+#TABLE 2
+
+#TABLE 3
+
+#TABLE 4
+
+#TABLE 5
+
+#TABLE 6
+
+#TABLE 7
+
+#TABLE 8
+
+#TABLE 9
+
+#TABLE 10
+
 
